3d39e1da46ba8d0cdfcabae1f0fa4196c50d6122
PaperGoose2566 <papergoose2566@gmail.com>
2024-06-04 21:44:39 +0200

    Random role assignment (not saved yet)

--- app/src/main/java/com/beboop/evildoggy/Game.kt
+++ app/src/main/java/com/beboop/evildoggy/Game.kt
@@ -4,13 +4,17 @@ import com.beboop.evildoggy.classes.Badangler
 import com.beboop.evildoggy.classes.Bidoofer
 import com.beboop.evildoggy.classes.Roles
 import com.beboop.evildoggy.classes.Werewolf
+import java.util.Random
 
 class Game {
     private var roles: MutableList<Roles>
 
     init {
         roles = mutableListOf<Roles>(Werewolf(), Bidoofer(), Badangler())
-        roles.shuffle()
+    }
+
+    fun randomRoleIdx() : Int {
+        return (0..(roles.size-1)).random()
     }
 
     fun getRoles() : List<Roles> {
--- app/src/main/java/com/beboop/evildoggy/MainActivity.kt
+++ app/src/main/java/com/beboop/evildoggy/MainActivity.kt
@@ -18,9 +18,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.beboop.evildoggy.ui.theme.EvilDoggyTheme
+import java.util.Random
 
+var usedNums = mutableListOf<Int>()
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    usedNums = mutableListOf<Int>()
                     DisplayRole()
                 }
             }
@@ -37,19 +39,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun randomRole() {
+
+}
+
 @Composable
 fun DisplayRole(modifier: Modifier = Modifier) {
-    var idx by remember{mutableIntStateOf(0) }
+    // ToDo: remember the role assignment order in a variable
     val game = Game()
+    // idx is a random index of the roles list
+    var idx by remember{mutableIntStateOf(game.randomRoleIdx())}
+    //bing counts how many times the thing has happened
+    var bing by remember { mutableIntStateOf(0)}
     Text(
         text = game.getRoles()[idx].name,
         modifier = modifier
     )
-    Button(onClick = {if(idx < game.getRoles().size-1) {
-        idx++
-    }}) {
-
-    }
+    Button(onClick = {
+        if(bing<game.getRoles().size-1) {
+            usedNums.add(idx)
+            while(usedNums.contains(idx)) {
+                idx = game.randomRoleIdx()
+            }
+            bing++
+        }
+    }) {} //funny brackets that break everything
 }
 
 @Preview(showBackground = true)
